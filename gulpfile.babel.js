1eb6362ffddc68f380bd86f1ef0984a58f634712
Renato de Leão <renatodeleao@gmail.com>
2019-02-22 11:48:35 +0000

    Added browser sync to go along with docs while developing

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,8 +9,9 @@ import del from "del";
 import cssnano from "cssnano";
 import sassdoc from "sassdoc"
 import moduleImporter from 'sass-module-importer';
+import browserSync from 'browser-sync';
 
-
+const server = browserSync.create();
 const paths = {
 	scss: {
 		src: "./scss",
@@ -35,7 +36,7 @@ function build(cb){
 		cssnano()
 	];
 
-	return src(`${paths.scss.src}/qnorr.scss`)
+	return src(`${paths.scss.src}/qnorr.scss`, { since: lastRun(build) })
 		.pipe(sourcemaps.init())
 		.pipe(scss({
 			importer: moduleImporter()
@@ -48,22 +49,42 @@ function build(cb){
 		.pipe(dest(paths.scss.dest))
 }
 
-export function sassDocs() {
+export function sassDocs(done) {
 	const options = {
 		dest: 'docs',
 		verbose: true,
 		//theme: "flippant"
 	};
+	const stream = sassdoc(options);
 
-	return src(`${paths.scss.src}/**/*.scss`)
-		.pipe(sassdoc(options));
+  src(`${paths.scss.src}/**/*.scss`)
+    .pipe(stream)
+    .on('end', function () {
+      console.log('End of parsing phase');
+    });
 
+  return stream.promise.then(function () {
+    console.log('End of documentation process');
+  });
 }
 
+function reload(done) {
+  server.reload();
+  done();
+}
+
+function serve(done) {
+  server.init({
+    server: {
+      baseDir: paths.docs.dest
+    }
+  });
+  done();
+}
 
 export function watching() {
-	watch(`${paths.scss.src}/**/*.scss`, parallel(build, sassDocs));
+	watch(`${paths.scss.src}/**/*.scss`, series(build, sassDocs, reload));
 }
 
-exports["dev"] = series(cleanDocs, clean, build, sassDocs, watching);
-export default series(clean, build);
+exports["dev"] = series(cleanDocs, clean, build, sassDocs, serve, watching);
+export default series(clean, build, sassDocs);
