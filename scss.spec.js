07623d29e8f87b100e809c915dde60d941ba84d7
Renato de Leão <renatodeleao@gmail.com>
2019-11-29 20:38:49 +0000

    Add dart sass as sass engine for testing
    required to add a jest config variable to package.json to overcome this issue
    https://github.com/oddbird/true/issues/156

--- scss.spec.js
+++ scss.spec.js
@@ -1,7 +1,10 @@
 const { runSass } = require('sass-true')
 const { sync } = require('glob');
+const sass =  require('sass');
 
 describe('SCSS', () => {
   const files = sync('scss/**/*.spec.scss')
-  files.forEach(file => runSass({ file }, {describe, it}) )
+  files.forEach(file => {
+    runSass({ file }, { sass, describe, it })
+  })
 })
